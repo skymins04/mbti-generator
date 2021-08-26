b39e8a67a8e65fb11451079a9a68d832ba481a24
skymins04 <skymin0417@gmail.com>
2021-08-27 02:10:24 +0900

    기본 틀 및 드레그 앤 무브 구현, 오브젝트별 id 구분으로 중첩현상 해결

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Draggable from "./Draggable.svelte";
+</script>
+
+<h1>drag object test</h1>
+<div id="viewport">
+  <Draggable left={50} top={50} objId={1}>
+    <h1>Drag Me 1</h1>
+  </Draggable>
+
+  <Draggable left={300} top={50} objId={2}>
+    <h1>Drag Me 2</h1>
+  </Draggable>
+
+  <Draggable left={50} top={300} objId={3}>
+    <h1>Drag Me 3</h1>
+  </Draggable>
+</div>
+
+<style>
+  #viewport {
+    width: 800px;
+    height: 500px;
+    margin: 10px;
+    border: 1px solid #ccc;
+    position: relative;
+    box-shadow: 0 0 3px 3px #ccc inset;
+  } /*
+  #block {
+    cursor: pointer;
+    width: 50px;
+    height: 50px;
+    background-color: coral;
+    border-radius: 10px;
+    position: absolute;
+  }*/
+</style>
---
+++ src/Draggable.svelte
@@ -0,0 +1,143 @@
+<script lang="ts">
+  import { afterUpdate } from "svelte";
+
+  export let left = 30;
+  export let top = 30;
+  export let width = 200;
+  export let height = 200;
+  export let objId = 1;
+  let moving = false;
+  let selected = false;
+  let resizing = false;
+  let selectedHandle = "";
+
+  function start() {
+    moving = true;
+  }
+
+  function stop() {
+    moving = false;
+    resizing = false;
+  }
+
+  function move(e) {
+    if (moving) {
+      if (resizing) {
+        if (selectedHandle == "top-left-handle") {
+          width -= e.movementX;
+          height -= e.movementY;
+          left += e.movementX;
+          top += e.movementY;
+        } else if (selectedHandle == "top-right-handle") {
+          width += e.movementX;
+          height -= e.movementY;
+          top += e.movementY;
+        } else if (selectedHandle == "bottom-left-handle") {
+          width -= e.movementX;
+          height += e.movementY;
+          left += e.movementX;
+          //top += e.movementY;
+        } else {
+          width += e.movementX;
+          height += e.movementY;
+        }
+      } else {
+        left += e.movementX;
+        top += e.movementY;
+      }
+    }
+  }
+
+  function select() {
+    if (selected) {
+      document.getElementById("handles" + objId.toString()).style.display =
+        "block";
+    } else {
+      document.getElementById("handles" + objId.toString()).style.display =
+        "none";
+    }
+  }
+
+  function handleSelect(e) {
+    resizing = true;
+    selectedHandle = e.target.id;
+  }
+
+  afterUpdate(async () => {
+    select();
+    console.log(resizing);
+  });
+</script>
+
+<svelte:window
+  on:click={() => {
+    selected = false;
+  }}
+  on:mouseup={stop}
+  on:mousemove={move}
+/>
+
+<section
+  on:mousedown={start}
+  on:click={(e) => {
+    e.stopPropagation();
+    selected = true;
+  }}
+  style="left: {left}px; top: {top}px; width: {width}px; height: {height}px;"
+  id="draggable"
+>
+  <div id={"handles" + objId.toString()}>
+    <span id="top-left-handle" on:mousedown={handleSelect} />
+    <span id="top-right-handle" on:mousedown={handleSelect} />
+    <span id="bottom-left-handle" on:mousedown={handleSelect} />
+    <span id="bottom-right-handle" on:mousedown={handleSelect} />
+  </div>
+  <slot id="slot" />
+</section>
+
+<style>
+  #draggable {
+    position: relative;
+    user-select: none;
+    position: absolute;
+    border: 1px solid gray;
+    cursor: move;
+  }
+
+  #draggable > div {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  #draggable > div > span {
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    border: 2px solid rgb(88, 168, 221);
+    background-color: white;
+  }
+
+  #top-left-handle {
+    top: -7px;
+    left: -7px;
+  }
+
+  #top-right-handle {
+    top: -7px;
+    right: -7px;
+  }
+
+  #bottom-left-handle {
+    bottom: -7px;
+    left: -7px;
+  }
+
+  #bottom-right-handle {
+    bottom: -7px;
+    right: -7px;
+  }
+</style>
